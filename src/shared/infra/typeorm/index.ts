dcc2617690c8abc24a8f38cb9a91b77e68d3bf6f
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-23 11:26:53 -0300

    refatorando a config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database_c-car'): Promise<Connection> => {
+export default async (host = `${process.env.DB_SERVICENAME}`): Promise<Connection> => {
   const connection = await getConnectionOptions();
 
   return createConnection(Object.assign(connection, { host }));
